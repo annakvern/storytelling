8dc9f9dc8057f0b3969df03fd726f99b2abfd916
annakvern <anna.kvernplassen@gmail.com>
2024-10-28 16:05:15 +0100

    add css file

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 //Sätt upp projektet likt dagens uppgift och skapa en funktion för varje "scen" i er berättelse som byter ut texten 
 //på sidan och vad som händer när man klickar på knapparna - dvs vilken scen som ska visas härnäst.
+window.addEventListener("DOMContentLoaded", main); 
+
+/** This is the starting point for the program */
+function main() {
+  // setupEventListeners();
+  // startClock();
+  // fetchUsersFromDatabase()
+}
+
+/** Register all events for buttons and other ui elements */
+function setupEventListeners() {
+  //burgerMenu.onclick = toggleHeaderOpen;
+  //zoom.onclick = toggleZoom;
+  // sätt upp fler knappar så dom klickbara.
+}
+
